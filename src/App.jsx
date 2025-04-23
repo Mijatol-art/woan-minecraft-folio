356889eddfe0a947cf43393bbfad2be1f28f8cf8
Andrew Woan <andrewwoan@gmail.com>
2025-04-23 07:14:51 -0400

    REMOVE DAT ANALYTICS BOI

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Modal from "./components/Modal/Modal";
 import AudioToggleButton from "./components/AudioToggleButton/AudioToggleButton";
 import LoadingScreen from "./components/LoadingScreen/LoadingScreen";
 import InfoButton from "./components/InfoButton/InfoButton";
-import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
